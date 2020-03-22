566a8f32e2ad211f1d92d28eb47ed6b930aa8432
Dawid Dziurla <dawidd0811@gmail.com>
2020-03-22 14:34:39 +0100

    simple http server

--- main.go
+++ main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+    "fmt"
+    "github.com/gorilla/mux"
+    "log"
+    "net/http"
+)
+
+
+func handleTest (w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintln(w, "response")
+}
+
+
 func main() {
+    router := mux.NewRouter()
+    router.HandleFunc("/", handleTest).Methods(http.MethodGet)
+    server := http.Server{
+        Addr:              "0.0.0.0:8080",
+        Handler:           router,
+        TLSConfig:         nil,
+    }
+    err := server.ListenAndServe()
+    if err != nil {
+        log.Fatal(err)
+    }
 }

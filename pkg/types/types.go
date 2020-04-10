8f9e02248723d2f368818af4176d2928ab075739
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-10 18:29:09 +0200

    types: make some fields types unsigned

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -7,11 +7,11 @@ type Status struct {
 
 type Sensors struct {
 	ID          uint    `json:"-"           sql:"column:id;primary_key;auto_increment"`
-	Timestamp   int64   `json:"timestamp"   sql:"column:timestamp;type:bigint(20)"`
+	Timestamp   int64   `json:"timestamp"   sql:"column:timestamp;type:bigint unsigned"`
 	Temperature float32 `json:"temperature" sql:"column:temperature;type:float(5,2)"`
-	Pressure    float32 `json:"pressure"    sql:"column:pressure;type:float(6,2)"`
-	Humidity    float32 `json:"humidity"    sql:"column:humidity;type:float(4,2)"`
-	Gas         float32 `json:"gas"         sql:"column:gas;type:float(8,2)"`
+	Pressure    float32 `json:"pressure"    sql:"column:pressure;type:float(6,2) unsigned"`
+	Humidity    float32 `json:"humidity"    sql:"column:humidity;type:float(4,2) unsigned"`
+	Gas         float32 `json:"gas"         sql:"column:gas;type:float(8,2) unsigned"`
 }
 
 type Notification struct {

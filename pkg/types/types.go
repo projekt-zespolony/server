24745896193394d122070850454c22b37d3576aa
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-10 18:43:46 +0200

    types: tweak

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,12 +6,12 @@ type Status struct {
 }
 
 type Sensors struct {
-	ID          uint    `json:"-"           sql:"column:id;primary_key;auto_increment"`
-	Timestamp   int64   `json:"timestamp"   sql:"column:timestamp;type:bigint unsigned"`
-	Temperature float32 `json:"temperature" sql:"column:temperature;type:float(5,2)"`
-	Pressure    float32 `json:"pressure"    sql:"column:pressure;type:float(6,2) unsigned"`
-	Humidity    float32 `json:"humidity"    sql:"column:humidity;type:float(4,2) unsigned"`
-	Gas         float32 `json:"gas"         sql:"column:gas;type:float(8,2) unsigned"`
+	ID          uint    `json:"-"           sql:"column:id;          type:int unsigned;        not null; primary_key; auto_increment;"`
+	Timestamp   int64   `json:"timestamp"   sql:"column:timestamp;   type:bigint unsigned;     not null;"`
+	Temperature float32 `json:"temperature" sql:"column:temperature; type:float(5,2) signed;   not null;"`
+	Pressure    float32 `json:"pressure"    sql:"column:pressure;    type:float(6,2) unsigned; not null;"`
+	Humidity    float32 `json:"humidity"    sql:"column:humidity;    type:float(4,2) unsigned; not null;"`
+	Gas         float32 `json:"gas"         sql:"column:gas;         type:float(8,2) unsigned; not null;"`
 }
 
 type Notification struct {

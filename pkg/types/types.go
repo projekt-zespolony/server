8794b5395ec54ce7e56df3fa72032206355541e2
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-16 17:48:57 +0200

    Revert "types: timestamp is an unsigned int"
    This reverts commit ab879cb35dde1efb4d23bd8dd4d072d5696daa05.
    
    `pkg/router/router.go:92:3: cannot use time.Now().UTC().Unix() (type
    int64) as type uint64 in field value`

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -7,7 +7,7 @@ type Status struct {
 
 type Sensors struct {
 	ID          uint    `json:"-"           sql:"column:id;          primary_key;              auto_increment"`
-	Timestamp   uint64  `json:"timestamp"   sql:"column:timestamp;   type:bigint unsigned;     not null"`
+	Timestamp   int64   `json:"timestamp"   sql:"column:timestamp;   type:bigint unsigned;     not null"`
 	Temperature float32 `json:"temperature" sql:"column:temperature; type:float(5,2) signed;   not null"`
 	Pressure    float32 `json:"pressure"    sql:"column:pressure;    type:float(6,2) unsigned; not null"`
 	Humidity    float32 `json:"humidity"    sql:"column:humidity;    type:float(4,2) unsigned; not null"`

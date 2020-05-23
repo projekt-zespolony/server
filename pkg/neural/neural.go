bf3cad51faa7e38452be42a26b285f362dacf372
Dawid Dziurla <dawidd0811@gmail.com>
2020-05-23 13:01:59 +0200

    pkg: add neural package

---
+++ pkg/neural/neural.go
@@ -0,0 +1,57 @@
+package neural
+
+import (
+	"github.com/projekt-zespolony/server/pkg/types"
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
+)
+
+const (
+	modelDir    = "model"
+	modelTag    = "tag"
+	treshold    = 0.5
+	inputLayer  = "input_input"
+	outputLayer = "output/Sigmoid"
+)
+
+func Predict(sensors *types.Sensors) (*types.OptimizationData, error) {
+	model, err := tf.LoadSavedModel(modelDir, []string{modelTag}, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer model.Session.Close()
+
+	data := [5]float32{
+		0,
+		sensors.Temperature,
+		sensors.Pressure,
+		sensors.Humidity,
+		sensors.Gas,
+	}
+
+	tensor, err := tf.NewTensor([][5]float32{data})
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := model.Session.Run(
+		map[tf.Output]*tf.Tensor{
+			model.Graph.Operation(inputLayer).Output(0): tensor,
+		},
+		[]tf.Output{
+			model.Graph.Operation(outputLayer).Output(0),
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	output := result[0].Value().([][]float32)[0]
+
+	opt := &types.OptimizationData{
+		PeopleInTheRoom:  output[0] > treshold,
+		WindowsAreOpened: output[1] > treshold,
+	}
+
+	return opt, nil
+}
